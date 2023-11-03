4799abb2d5fbdb8cce3bf12b44a0a936220b9436
asmaagamal119 <asmaagamal1109@gmail.com>
2023-11-03 13:43:58 -0700

    add

---
+++ 0x07-python-test_driven_development/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+three_dimensional_array = [
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+    ],
+    [
+        [7, 8, 9],
+        [10, 11, 12],
+    ],
+    [
+        [13, 14, 15],
+        [16, 17, 18],
+    ]
+]
+ 
+
+print(matrix_divided([[]],2))
+print()
+
